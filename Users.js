0a195f4851d5ec128c45ad3bb35ab91ee4afebf3
MKHOIRI19 <mkhoiri@student.uir.ac.id>
2024-01-19 20:02:32 +0700

    bebas ini pesan commit

---
+++ Users.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema(
+      {
+            nama: String,
+            email: { type: String, unique: true },
+            npm: String,
+            password: String
+      },
+      {
+            collection: "Users"
+      }
+);
+
+mongoose.model("Users", UserSchema);
+
+
+const ScheduleSchema = new mongoose.Schema(
+      {
+        
+        tgl: { type: Date },
+        hari: {
+          type: String,
+          enum: ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'],
+        },
+        amount: { Number} ,
+      },
+      {
+        collection: "Schedules",
+      }
+    );
+
+mongoose.model("Schedules", ScheduleSchema);
